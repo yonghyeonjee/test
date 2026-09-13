896b795121ba912026ce3f3c3ded744c8dde3f90
yonghyeonjee <yonghyeonjee@gmail.com>
2026-09-13 21:30:37 +0900

    Update robots.ts

--- web/app/robots.ts
+++ web/app/robots.ts
@@ -8,6 +8,3 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: `${SITE}/sitemap.xml`,
   };
 }
-
-
-#DaumWebMasterTool:c5c205ec2131c804e16bafa3971f389b5c56e8e4b4e42a153a178f743f21252d:TTrNsDNWeyJBX4OQHE95rw==
